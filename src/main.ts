680d31c9b07009bd8ee02bd42c335732aa2df732
demitas <demitas@tutanota.com>
2024-12-27 13:14:02 +0900

    add WebView

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import * as path from 'path';
 function createWindow(): void {
     const win = new BrowserWindow({
         width: 1200,
-        height: 800
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true  // WebViewを有効化
+        }
     });
 
     win.loadFile(path.join(__dirname, '../src/index.html'));
